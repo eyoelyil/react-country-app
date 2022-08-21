8f5577aa3f5f363845c7611aad9b836a472495a4
Mulate Eyoel <s2200125@BC-C02GR0BRQ6LR-MacBook-Air.local>
2022-08-21 15:08:35 +0300

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [countries, setCountries] = useState([
+    {
+      name: "Afghanistan",
+      languages: "Kabul",
+      currenies: "Afghani",
+      population: "35000000",
+      id: "1",
+    },
+    {
+      name: "Albania",
+      languages: "Tirana",
+      currenies: "Lek",
+      population: "3300000",
+      id: "2",
+    },
+    {
+      name: "Algeria",
+      languages: "Algiers",
+      currenies: "Algerian Dinar",
+      population: "35000000",
+      id: "3",
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {countries.map((country) => (
+        <div className="country-preview" key={country.id}>
+          <h2>{country.name}</h2>
+          <p>Language - {country.languages}</p>
+          <p>Currency - {country.currenies}</p>
+          <p>Population - {country.population}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Country App</h1>
+      <h1>Country App</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/">Countries</a>
